ab87cb73907c7cc6d013318d553bb6d2eac04abc
Carles Mata <info@appsinet.com>
2018-07-10 11:03:33 +0200

    After updating server and postal, wasn't working without `sudo -u postal`

--- postal-zabbix.sh
+++ postal-zabbix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+cd /opt/postal/app
 status=$(/opt/postal/app/bin/postal status)
-#status=$(cat /home/carles/Downloads/postalCommand.txt)
 if [ "$#" -lt 1 ]; then
   #Postal server start time
   started=$(echo "${status}" | grep 'Started' | xargs | cut -d ' ' -f 2,3 | cut -d 'm' -f 2)
